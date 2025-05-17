045e8fd175a772efc85632198d2a990ae35157d4
sung2708 <aki18082002@gmail.com>
2025-05-17 14:34:54 +0700

    add post and change tiktok channel name

--- pages/posts.js
+++ pages/posts.js
@@ -6,39 +6,45 @@ import { GridItem } from '../components/grid-item'
 import nvim from '../public/images/posts/nvim.png'
 import tmux from '../public/images/posts/tmux.png'
 import leetcode from '../public/images/posts/leetcode.png'
+import Docker from '../public/images/posts/docker.jpeg'
 
 const Posts = () => (
-    <Layout title="Posts">
-        <Container>
-            <Heading as="h3" fontSize={20} mb={4}>
-                Popular posts
-            </Heading>
-            <Section delay={0.3}>
-                <SimpleGrid columns={[1, 2, 2]} gap={6}>
-                    <GridItem
-                        title="Giới thiệu về vim/neovim"
-                        thumbnail={nvim}
-                        href="https://www.tiktok.com/@dev_ui/video/7480185148848278802"
-                    ></GridItem>
-                    <GridItem
-                        title="Giới thiệu Tmux"
-                        thumbnail={tmux}
-                        href="https://www.tiktok.com/@dev_ui/video/7480965886996663559"
-                    ></GridItem>
-                    <GridItem
-                        title="LeetCode problem rotate-image"
-                        thumbnail={leetcode}
-                        href="https://www.tiktok.com/@dev_ui/video/7456841446667095314"
-                    ></GridItem>
-                    <GridItem
-                        title="LeetCode problem longest-palindromic-substring"
-                        thumbnail={leetcode}
-                        href="https://www.tiktok.com/@dev_ui/video/7483403447375564039"
-                    ></GridItem>
-                </SimpleGrid>
-            </Section>
-        </Container>
-    </Layout>
+  <Layout title="Posts">
+    <Container>
+      <Heading as="h3" fontSize={20} mb={4}>
+        Popular posts
+      </Heading>
+      <Section delay={0.3}>
+        <SimpleGrid columns={[1, 2, 2]} gap={6}>
+          <GridItem
+            title="Giới thiệu về vim/neovim"
+            thumbnail={nvim}
+            href="https://www.tiktok.com/@thelazycode_r/video/7480185148848278802"
+          ></GridItem>
+          <GridItem
+            title="Giới thiệu Tmux"
+            thumbnail={tmux}
+            href="https://www.tiktok.com/@thelazycode_r/video/7480965886996663559"
+          ></GridItem>
+          <GridItem
+            title="LeetCode problem rotate-image"
+            thumbnail={leetcode}
+            href="https://www.tiktok.com/@thelazycode_r/video/7456841446667095314"
+          ></GridItem>
+          <GridItem
+            title="LeetCode problem longest-palindromic-substring"
+            thumbnail={leetcode}
+            href="https://www.tiktok.com/@thelazycode_r/video/7483403447375564039"
+          ></GridItem>
+          <GridItem
+            title="Docker Introduction"
+            thumbnail={Docker}
+            href="https://www.tiktok.com/@thelazycode_r/video/7504866717999336712"
+          ></GridItem>
+        </SimpleGrid>
+      </Section>
+    </Container>
+  </Layout>
 )
 
 export default Posts

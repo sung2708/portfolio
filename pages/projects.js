d1e47f7459fcd066eda8f356bfe5e98289a2c93b
sung2708 <aki18082002@gmail.com>
2025-06-05 14:23:36 +0700

    Change project page status

--- pages/projects.js
+++ pages/projects.js
@@ -1,41 +1,52 @@
-import { Container, Box, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
-import Section from '../components/section'
-import { ProjectGridItem } from '../components/grid-item'
-import thumbTaskApp from '../public/images/projects/task-app.png'
-import thumbVqa from '../public/images/projects/bone-vqa.jpg'
-import Layout from '../components/layouts/article'
+// import { Container, Box, Heading, SimpleGrid, Divider } from '@chakra-ui/react'
+// import Section from '../components/section'
+// import { ProjectGridItem } from '../components/grid-item'
+// import thumbTaskApp from '../public/images/projects/task-app.png'
+// import thumbVqa from '../public/images/projects/bone-vqa.jpg'
+// import Layout from '../components/layouts/article'
+//
+// const Projects = () => {
+//     return (
+//         <Layout title="Projects">
+//             <Container>
+//                 <Heading as="h3" fontSize={20} mb={4}>
+//                     Projects
+//                 </Heading>
+//                 <SimpleGrid columns={[1, 1, 2]} gap={6}>
+//                     <Section>
+//                         <ProjectGridItem
+//                             id="task-app"
+//                             title={'Task App'}
+//                             thumbnail={thumbTaskApp}
+//                         >
+//                             Personal Projects
+//                         </ProjectGridItem>
+//                     </Section>
+//                     <Section>
+//                         <ProjectGridItem
+//                             id="bone-vqa"
+//                             title={'Bone VQA'}
+//                             thumbnail={thumbVqa}
+//                         >
+//                             Graduation thesis
+//                         </ProjectGridItem>
+//                     </Section>
+//                 </SimpleGrid>
+//             </Container>
+//         </Layout>
+//     )
+// }
+//
+// export default Projects
+// export { getServerSideProps } from '../components/chakra'
 
 const Projects = () => {
-    return (
-        <Layout title="Projects">
-            <Container>
-                <Heading as="h3" fontSize={20} mb={4}>
-                    Projects
-                </Heading>
-                <SimpleGrid columns={[1, 1, 2]} gap={6}>
-                    <Section>
-                        <ProjectGridItem
-                            id="task-app"
-                            title={'Task App'}
-                            thumbnail={thumbTaskApp}
-                        >
-                            Personal Projects
-                        </ProjectGridItem>
-                    </Section>
-                    <Section>
-                        <ProjectGridItem
-                            id="bone-vqa"
-                            title={'Bone VQA'}
-                            thumbnail={thumbVqa}
-                        >
-                            Graduation thesis
-                        </ProjectGridItem>
-                    </Section>
-                </SimpleGrid>
-            </Container>
-        </Layout>
-    )
+  return (
+    <div>
+      <h1>Projects</h1>
+      <p>Coming soon...</p>
+    </div>
+  )
 }
 
 export default Projects
-export { getServerSideProps } from '../components/chakra'
